ae77909000a3a1ee77c474fdb29c3455e8b45824
Difelice-e <difelice.emanuele@gmail.com>
2022-05-03 17:16:59 +0200

    installato bootstrap + show

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="{{asset('css/app.css')}}">
+  <title>laravel-base-crud</title>
+</head>
+<body>
+  <div class="container my-3">
+    <a href="{{route('comics.index')}}" class="p-2 btn-secondary border my-5 rounded">Comics List</a>
+  </div>
+
+  @yield('content')
+
+  
+</body>
+</html>
